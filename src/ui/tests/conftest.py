ced7597fdcb194c1ce35ef4cc4d1886d7f784635
DaryaPopova <pd2405@yandex.ru>
2023-08-13 15:56:08 +0200

    fixed problem with interactable in headless https://stackoverflow.com/questions/47776774/element-is-not-clickable-at-point-in-headless-mode-but-when-we-remove-headless

--- src/ui/tests/conftest.py
+++ src/ui/tests/conftest.py
@@ -23,6 +23,7 @@ def driver():
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--disable-notifications')
+    chrome_options.add_argument('--window-size=1920,1080')
     driver = webdriver.Chrome(chrome_options)
     driver.maximize_window()
 
