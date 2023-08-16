536ee0560dbf6de71bfda5a7f667a47139a16251
XeniaBerkut <x.ivannikova@gmail.com>
2023-08-16 18:59:58 +0400

    - Added test to check behavior when doesn't fill any fields - Moved urls to enum

--- src/ui/tests/registration/test_registration.py
+++ src/ui/tests/registration/test_registration.py
@@ -5,6 +5,7 @@ import pytest
 
 from ui.entities.user import User
 from ui.enums.registration_controls_texts import RegistrationControlsTexts
+from ui.enums.urls import URLS
 from ui.pages.web_trading_page import WebTradingPage
 from ui.tests.conftest import driver, logger
 from ui.pages.home_page import HomePage
@@ -12,6 +13,7 @@ from ui.enums.registration_buttons import RegistrationButtons
 from ui.pages.registration_page import RegistrationPage
 from selenium.webdriver.chrome.webdriver import WebDriver
 from helpers.test_data_helpers import make_test_data_uniq
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def get_data(test_data_file_name: str) -> dict:
@@ -28,7 +30,7 @@ data_registration: dict = get_data("test_registration_data_user.json")
                          data_registration,
                          ids=[data["test_case_title"] for data in data_registration])
 def test_registration(driver: WebDriver, test_case: dict):
-    driver.get("https://my.exness.com/accounts/sign-up")
+    driver.get(URLS.REGISTRATION_PAGE.value)
 
     test_case["data"]["email"] = make_test_data_uniq(test_case["data"]["email"])
     user = User(**test_case["data"])
@@ -36,7 +38,7 @@ def test_registration(driver: WebDriver, test_case: dict):
 
     web_trading_page: WebTradingPage = registration_page.fill_form_success(driver, user)
     web_trading_page.wait_welcome_dialog()
-    assert web_trading_page.driver.current_url == 'https://my.exness.com/webtrading/', \
+    assert web_trading_page.driver.current_url == URLS.WEB_TRADING_PAGE, \
         (f'Expected WebTradingPage, but was {web_trading_page.driver.current_url}. Maybe, because of the captcha,'
          f' as this is a pet project I cannot fix it')
 
@@ -50,7 +52,7 @@ data_pwd_controls: dict = get_data("test_registration_data_pwd_controls.json")
                          ids=[data["test_case_title"] for data in data_pwd_controls])
 def test_registration_form_password_controls(driver: WebDriver, test_case: dict):
     logging.info('Go to the RegistrationPage')
-    driver.get("https://my.exness.com/accounts/sign-up")
+    driver.get(URLS.REGISTRATION_PAGE.value)
 
     logging.info(f'Create user from test data {test_case["data"]}')
     user = User(**test_case["data"])
@@ -60,10 +62,8 @@ def test_registration_form_password_controls(driver: WebDriver, test_case: dict)
     registration_page: RegistrationPage = registration_page.fill_form_failure(driver, user)
 
     logging.info('Check if registration is not completed and we are still on the RegistrationPage')
-    registration_page_url: str = driver.current_url
-    expected_static_url_part: str = 'https://my.exness.com/accounts/sign-up'
-    assert registration_page_url.startswith(expected_static_url_part), \
-        f'Expected RegistrationPage, but was {registration_page_url}'
+    assert EC.url_contains(URLS.REGISTRATION_PAGE.value), \
+        f'Expected RegistrationPage, but was {driver.current_url}'
 
     for control in test_case["controlColor"]:
         expected_color = test_case["controlColor"][control]
@@ -74,10 +74,23 @@ def test_registration_form_password_controls(driver: WebDriver, test_case: dict)
         assert control_text_color == expected_color, f'Expected {expected_color} color, but was {control_text_color}'
 
 
+@pytest.mark.order(2)
+def test_registration_form_empty_fields(driver: WebDriver):
+    logging.info('Go to the RegistrationPage')
+    driver.get(URLS.REGISTRATION_PAGE.value)
+
+    registration_page: RegistrationPage = RegistrationPage(driver)
+    logging.info('Click registration button')
+    registration_page.confirm_registration()
+    # TODO Add asserts for controls
+    assert EC.url_contains(URLS.REGISTRATION_PAGE.value),\
+        f'Expected RegistrationPage, but was {driver.current_url}'
+
+
 @pytest.mark.order(3)
 @pytest.mark.parametrize("btn", list(RegistrationButtons))
 def test_registration_buttons(driver: WebDriver, btn: RegistrationButtons):
-    driver.get("https://www.exness.com/")
+    driver.get(URLS.HOME_PAGE.value)
     home_page: HomePage = HomePage(driver)
 
     logger.info(f'Go to the Registration page by clicking {btn}')
@@ -87,11 +100,8 @@ def test_registration_buttons(driver: WebDriver, btn: RegistrationButtons):
     assert registration_page.check_title
 
     logger.info('Check if url of RegistrationPage is correct')
-    registration_page_url: str = registration_page.driver.current_url
-    expected_static_url_part: str = 'https://my.exness.com/accounts/sign-up'
-    # TODO use url_contains from EC
-    assert registration_page_url.startswith(expected_static_url_part),\
-        f'Expected RegistrationPage, but was {registration_page_url}'
+    assert EC.url_contains(URLS.REGISTRATION_PAGE.value),\
+        f'Expected RegistrationPage, but was {driver.current_url}'
 
     logger.info('Check if header of registration form is correct')
     assert registration_page.check_header()
